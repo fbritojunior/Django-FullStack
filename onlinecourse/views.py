0d5e57d7c1c3ff11e9b9eec137b584381030d569
Francisco Brito Jr <fbritojnr@gmail.com>
2023-05-03 14:09:59 -0400

    commit

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -113,19 +113,27 @@ def enroll(request, course_id):
          # Redirect to show_exam_result with the submission id
 #def submit(request, course_id):
 def submit(request, course_id):
-    Enrollment.objects.get(user=user, course=course)
-    Submission.objects.create(enrollment=enrollment)
+   
+   #if user.is_authenticated:
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    extract_answers = extract_answers(request)
+    Submission.objects.create(user=user, course=course, enrollment=enrollment, choices=choices, answer=extract_answers, mode='honor')
+    
+    
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(submission.id,)))
+        
     
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+    submitted_anwsers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwsers.append(choice_id)
+    return submitted_anwsers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
@@ -134,7 +142,11 @@ def submit(request, course_id):
         # Get the selected choice ids from the submission record
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+def show_exam_result(request, course_id, submission_id):
+    course = get_object_or_404(Course, pk=course_id)
+    submission = submission_id.user
+    choices = submission_id.choices
+    
 
 
 
